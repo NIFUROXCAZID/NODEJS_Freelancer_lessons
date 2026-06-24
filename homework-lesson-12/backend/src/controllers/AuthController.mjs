2310a8e1e29bac75c215bfd20d0abb68c1e99ae6
Amosov Vladislav <63251628+NIFUROXCAZID@users.noreply.github.com>
2026-06-24 18:24:40 +0300

    Update AuthController.mjs

--- homework-lesson-12/backend/src/controllers/AuthController.mjs
+++ homework-lesson-12/backend/src/controllers/AuthController.mjs
@@ -30,14 +30,20 @@ class AuthController {
       res.cookie('accessToken', tokens.accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite:
+          process.env.NODE_ENV === 'production'
+            ? 'none'
+            : 'lax',
         maxAge: 15 * 60 * 1000,
       })
 
       res.cookie('refreshToken', tokens.refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite:
+          process.env.NODE_ENV === 'production'
+            ? 'none'
+            : 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
 
